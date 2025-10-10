86d86262406edc5e5c37b4dee2745c5362c59f1a
ashfaq-viva <ashfaq.ahmed@vivasoftltd.com>
2025-10-11 04:04:07 +0600

    viewport updated

--- utils/viewports.js
+++ utils/viewports.js
@@ -1,10 +1,10 @@
 // utils/viewports.js
 
 export const allViewports = {
-  Desktop: { width: 1800, height: 1000 },
-  Laptop:  { width: 1440, height: 1000 },
+  Desktop: { width: 1440, height: 1000 },
+  Laptop:  { width: 1230, height: 1000 },
   Tablet:  { width: 768,  height: 1000 },
-  Mobile:  { width: 360,  height: 1000 }
+  Mobile:  { width: 412,  height: 1000 }
 };
 
 // Named objects ready to use in tests
