643c324eead7b084a76b585cca8233266cd15c66
ashfaq-viva <ashfaq.ahmed@vivasoftltd.com>
2025-10-16 19:46:31 +0600

    updated projects name

--- playwright.config.js
+++ playwright.config.js
@@ -98,12 +98,12 @@ export default defineConfig({
     // },
     
     /*{
-      name: 'firefox',
+      name: 'Browser:Firefox',
       use: { ...devices['Desktop Firefox'] },
     },
 
     {
-      name: 'webkit',
+      name: 'Browser: Webkit',
       use: { ...devices['Desktop Safari'] },
     },*/
 
@@ -119,7 +119,7 @@ export default defineConfig({
 
     /* Test against branded browsers. */
     // {
-    //   name: 'Microsoft Edge',
+    //   name: 'Browser: Microsoft Edge',
     //   use: { channel: 'msedge' },
     // },
     // {
