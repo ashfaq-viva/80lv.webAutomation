7a4b4c6e78f1afe5cc5141868b020e98fb673d78
ashfaq-viva <ashfaq.ahmed@vivasoftltd.com>
2025-10-22 16:31:50 +0600

    added partnersApi

--- api/apiMap.js
+++ api/apiMap.js
@@ -13,16 +13,14 @@ const defaultMethods = {
 
 const apiMap = {
   "80LV_PROD": {
-    cookieApi: { url: "https://cdn.80.lv/api", methods: { ...defaultMethods } },
-    loginApi: { url: "https://login.xsolla.com/api/login",methods: { ...defaultMethods },
+    loginApi: { url: "https://login.xsolla.com/api/login",methods: { ...defaultMethods }},
     signupApi: { url:"https://login.xsolla.com/api/user",methods: { ...defaultMethods }},
-    }
-  },
+    partnersApi: { url:"https://80.lv/api/partners",methods: { ...defaultMethods }},
+    },
   "80LV_QA": {
-    cookieApi: { url: "https://cdn.80.lv/api", methods: { ...defaultMethods } },
-    loginApi: { url: "https://login.xsolla.com/api/login",methods: { ...defaultMethods },
+    loginApi: { url: "https://login.xsolla.com/api/login",methods: { ...defaultMethods }},
     signupApi: { url:"https://login.xsolla.com/api/user",methods: { ...defaultMethods }},
-    }
+    partnersApi: { url:"https://80.lv/api/partners",methods: { ...defaultMethods }},
   }
 };
 
